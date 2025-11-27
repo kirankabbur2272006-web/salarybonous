14f309a3ae31ec943e82ff953ffff7144bdf8e1f
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-11-27 14:43:38 +0530

    Create sal.py

---
+++ sal.py
@@ -0,0 +1,9 @@
+# salary_bonus.py
+
+salary = float(input("Enter employee salary: "))
+
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print(f"Bonus Amount: {bonus}")
+print(f"Total Salary after Bonus: {total_salary}")
